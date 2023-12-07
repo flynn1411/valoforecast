0e69f564b67f0ad74d976dd0d0accbd8e7b7570f
juliamartinezhn <102008430+juliamartinezhn@users.noreply.github.com>
2023-12-06 18:01:29 -0600

    Lógica de predicción de puntos de próximas partidas

--- src/components/RankPrediction/RankPrediction.tsx
+++ src/components/RankPrediction/RankPrediction.tsx
@@ -1,7 +1,32 @@
 import HomeButton from "../HomeButton";
-
+import calculatePoints from "./logica";
 
 function RankPrediction(){
+  // Ejemplo de uso
+  //Info dada de las partidas
+  const info = [
+    [24, 14, 5, 74, 6, 2, 0],
+    [11, 12, 3, 66, 1, 1, 0],
+    [24, 13, 5, 75, 3, 1, 0],
+    [13, 17, 10, 60, 2, 1, 0],
+    [25, 17, 2, 75, 5, 1, 0],
+    [10, 18, 6, 57, 1, 1, 1],
+    [24, 15, 7, 71, 5, 1, 0],
+  ];
+  
+  // Puntos de partidas históricos
+  const points = [
+    [24],
+    [20],
+    [29],
+    [-19],
+    [-13],
+    [-21],
+    [15],
+  ];
+  
+  console.log('Predicciones de Puntos para Nuevas Partidas => ', calculatePoints(info, points));
+
   return(
     <div>
       Rank Prediction
--- src/components/RankPrediction/logica.ts
+++ src/components/RankPrediction/logica.ts
@@ -1,10 +1,145 @@
+import * as math from 'mathjs';
+import * as jStat from 'jstat';
+import MLR from "ml-regression-multivariate-linear";
 
-function Hola():number{
-  return 1;
+
+function calculateMeanStandarDeviation(matchData: {
+  kill: number[];
+  deaths: number[];
+  assists: number[];
+  econ: number[];
+  firstBlood: number[];
+  spikes: number[];
+  defuses: number[];
+}): {
+  killData: {
+    killMean: number,
+    killStandardDeviation: any
+  };
+  deathData: {
+    deathMean: number,
+    deathStandarDeviation: any
+  };
+  assistData: {
+    assistMean: number,
+    assistStandardDeviation: any
+  };
+  econData: {
+    econMean: number,
+    econStandardDeviation: any
+  };
+  firstBloodData: {
+    firstBloodMean: number,
+    firstBloodStandardDeviation: any
+  };
+  spikeData: {
+    spikeMean: number,
+    spikeStandardDeviation: any
+  };
+  defusesData: {
+    defusesMean: number,
+    defusesStandardDeviation: any
+  };
+} | null {
+  if (
+    matchData.kill.length === 0 ||
+    matchData.deaths.length === 0 ||
+    matchData.assists.length === 0 ||
+    matchData.econ.length === 0 ||
+    matchData.firstBlood.length === 0 ||
+    matchData.spikes.length === 0 ||
+    matchData.defuses.length === 0
+  ) {
+    return null;
+  }
+
+  const killMean = math.mean(matchData.kill);
+  const killStandardDeviation = math.std(matchData.kill);
+
+  const deathMean = math.mean(matchData.deaths);
+  const deathStandarDeviation = math.std(matchData.deaths);
+
+  const assistMean = math.mean(matchData.assists);
+  const assistStandardDeviation = math.std(matchData.assists);
+
+  const econMean = math.mean(matchData.econ);
+  const econStandardDeviation = math.std(matchData.econ);
+
+  const firstBloodMean = math.mean(matchData.firstBlood);
+  const firstBloodStandardDeviation = math.std(matchData.firstBlood);
+
+  const spikeMean = math.mean(matchData.spikes);
+  const spikeStandardDeviation = math.std(matchData.spikes);
+
+  const defusesMean = math.mean(matchData.defuses);
+  const defusesStandardDeviation = math.std(matchData.defuses);
+
+  return {
+    killData: { killMean, killStandardDeviation },
+    deathData: { deathMean, deathStandarDeviation },
+    assistData: { assistMean, assistStandardDeviation },
+    econData: { econMean, econStandardDeviation },
+    firstBloodData: { firstBloodMean, firstBloodStandardDeviation },
+    spikeData: { spikeMean, spikeStandardDeviation },
+    defusesData: { defusesMean, defusesStandardDeviation }
+  };
 }
 
-export function Mundo():number{
-  return 2;
+function generateRandomData(
+  killData: { killMean: number; killStandardDeviation: number },
+  deathData: { deathMean: number; deathStandarDeviation: number },
+  assistData: { assistMean: number; assistStandardDeviation: number },
+  econData: { econMean: number; econStandardDeviation: number },
+  firstBloodData: { firstBloodMean: number; firstBloodStandardDeviation: number },
+  spikeData: { spikeMean: number; spikeStandardDeviation: number },
+  defusesData: { defusesMean: number; defusesStandardDeviation: number }, rowNumber: number
+): number[][] {
+  const generatedData: number[][] = [];
+
+  //const rowNumber = Object.keys();
+  for (let i = 0; i < rowNumber; i++) {
+    const filaGenerada: number[] = [
+      Math.abs(Math.round(jStat.normal.inv(Math.random(), killData.killMean, killData.killStandardDeviation))),
+      Math.abs(Math.round(jStat.normal.inv(Math.random(), deathData.deathMean, deathData.deathStandarDeviation))),
+      Math.abs(Math.round(jStat.normal.inv(Math.random(), assistData.assistMean, assistData.assistStandardDeviation))),
+      Math.abs(Math.round(jStat.normal.inv(Math.random(), econData.econMean, econData.econStandardDeviation))),
+      Math.abs(Math.round(jStat.normal.inv(Math.random(), firstBloodData.firstBloodMean, firstBloodData.firstBloodMean))),
+      Math.abs(Math.round(jStat.normal.inv(Math.random(), spikeData.spikeMean, spikeData.spikeStandardDeviation))),
+      Math.abs(Math.round(jStat.normal.inv(Math.random(), defusesData.defusesMean, defusesData.defusesStandardDeviation)))
+    ];
+
+    generatedData.push(filaGenerada);
+  }
+
+  return generatedData;
 }
 
-export default Hola;
+function transformInfo(info: any): any {
+  const properties: string[] = ['kill', 'deaths', 'assists', 'econ', 'firstBlood', 'spikes', 'defuses'];
+
+  const data: any = properties.reduce((acc: any, prop: string, index: number) => {
+    acc[prop] = info.map((partida: any) => partida[index]);
+    return acc;
+  }, {});
+
+  return data;
+}
+
+function calculatePoints(originalInfo: any, points: number[][]): any {
+  const data = transformInfo(originalInfo)
+  const result = calculateMeanStandarDeviation(data);
+
+  if (result?.killData) {
+    const randomData = generateRandomData(result?.killData, result?.deathData, result?.assistData, result?.econData, result?.firstBloodData, result?.spikeData, result?.defusesData, data.kill.length);
+
+    // predicción
+
+    return new MLR(originalInfo, points).predict(randomData);
+
+  }
+
+}
+
+
+
+export default calculatePoints;
