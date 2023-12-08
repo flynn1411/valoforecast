bde2c8ea368ff67e1528978995b4729775fdff2c
Josue Izaguirre <flynn1411@gmail.com>
2023-12-08 00:51:26 -0600

    Ingreso de Partidas Commit

---
+++ src/interfaces/Partida.ts
@@ -0,0 +1,11 @@
+export default interface Partida{
+  kills: number,
+  deaths: number,
+  assists: number,
+  econ: number,
+  first_bloods: number,
+  defuses: number,
+  plants: number,
+  RR: number,
+  date: string
+}
